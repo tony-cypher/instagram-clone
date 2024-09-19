d9b35bbce626a45fb712233cef6a2c8ba14fdb66
Cipher <tonycypher0@gmail.com>
2024-09-19 17:05:56 +0100

    railway deployment 2

--- social/settings.py
+++ social/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -134,13 +133,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # For image uploads
 MEDIA_URL = '/media/'
