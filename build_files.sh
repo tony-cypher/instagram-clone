8d18a1e9eac4c8456a6ebd82957b6d2295d3ef12
Cipher <tonycypher0@gmail.com>
2024-09-19 08:42:46 +0100

    resolve build error 1

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# install python in the vercel environment
+apt-get update && apt-get install -y python3 python3-pip
+
+# install required packages
+pip3 install -r requirements.txt
+
+# Run Django collectstatic
+python3.9 manage.py collectstatic --noinput
