51fbd85347106a48cd4798aebd7863e632c3ab13
Cipher <tonycypher0@gmail.com>
2024-09-21 09:55:57 +0100

    Adjusted Login and Register

--- users/forms.py
+++ users/forms.py
@@ -4,14 +4,9 @@ from django.contrib.auth.models import User
 
 
 class RegistrationForm(UserCreationForm):
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control my-2', 'placeholder': 'Password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control my-2', 'placeholder': 'Confirm Password'}))
+    password1 = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'password1', 'password2')
-        widgets = {
-            'username': forms.TextInput(attrs={'class':'form-control my-2', 'placeholder': 'Username'}),
-            'email': forms.EmailInput(attrs={'class':'form-control my-1', 'placeholder': 'Email'}),
-            'first_name': forms.TextInput(attrs={'class':'form-control my-2', 'placeholder': 'First Name'}),
-        }
